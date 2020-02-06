c42569305d2d87fddb2e1bad6e3c6b379dd1bb71
sevewing <liweisi8121@hotmail.com>
2020-02-06 23:57:12 +0100

    cleaned windmills

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import paramiko
 import pyarrow.parquet as pq
+import pandas as pd
 import os
  
 keyfile = os.path.expanduser('/Users/Levis/.ssh/itu.pem')
@@ -12,10 +13,12 @@ ssh.connect('orkneycloud.itu.dk', username='liwb', password='ch4ng3m3', pkey = k
 ftp_client= ssh.open_sftp()
 remote_file = ftp_client.open('/home/sebastian/energinet/ITU_DATA/masterdatawind.parquet')
 
-windmill = pq.read_pandas(remote_file).to_pandas()
+windmill = pq.read_table(remote_file).to_pandas()
+windmill.to_csv('data/windmills.csv',index=False)
 
 ftp_client.close()
 ssh.close()
 
-# import pysftp
-# sftp = pysftp.Connection(host = 'orkneycloud.itu.dk', username="liwb", private_key = '/Users/Levis/.ssh/itu.pem')
+
+
+
