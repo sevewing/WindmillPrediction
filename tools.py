b9d84e5642eed81f85d37e09ad1425d094b90dfb
sevewing <liweisi8121@hotmail.com>
2020-02-21 23:09:16 +0100

    update

--- tools.py
+++ tools.py
@@ -1,6 +1,6 @@
 from math import log,sqrt,atan2,pi,cos,sin
 
-pow_law = lambda v10, v100, z_hat, flag : flag * v100 * ((z_hat/100)**(log(v100/(v10+1e-06)+1e-06)/log(100/10)))
+pow_law = lambda v10, v100, z_hat, flag : flag * v100 * ((z_hat/100)**(log(v100/(v10+1e-06)+1e-06)/log(100/10)))+1e-06
 
 
 def _wind_speed_to_vector(s, d):
@@ -8,7 +8,7 @@ def _wind_speed_to_vector(s, d):
     v = s * sin(pi/180 * (270-d))
     return u, v
 
-def _wind_interpolation(s1, s2, z):
+def _pow_interpolation(s1, s2, z):
     flag = 1
 
     if s1 < 0 and s2 > 0:
@@ -22,9 +22,14 @@ def _wind_interpolation(s1, s2, z):
     return w_interp
 
 
-def wind_convert(s1, d1, s2, d2, z):
-    u1, v1 = _wind_speed_to_vector(s1, d1)
-    u2, v2 = _wind_speed_to_vector(s2, d2)
-    u_interp = _wind_interpolation(u1, u2, z)
-    v_interp = _wind_interpolation(v1, v2, z)
-    return u_interp, v_interp
+def wind_interp(s1, d1, s2, d2, z):
+    u_i, v_i = 0.0, 0.0
+    try:
+        u1, v1 = _wind_speed_to_vector(s1, d1)
+        u2, v2 = _wind_speed_to_vector(s2, d2)
+        u_i = _pow_interpolation(u1, u2, z)
+        v_i = _pow_interpolation(v1, v2, z)
+    except:
+        print("!!!!!!!: ", type(z))
+
+    return u_i, v_i
