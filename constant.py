f1429a07997b2341a26d47ae2853010b5a3650fe
sevewing <liweisi8121@hotmail.com>
2020-06-26 13:29:27 +0200

    Single windmill model

--- constant.py
+++ constant.py
@@ -1,28 +1,71 @@
 from pyspark.sql.types import *
 
-# windmill_path = "./windmill_cleaned.csv"
-# settlement_path = "/datasets/energinet/settlement/2019.parquet"
-# ws10_path = "/datasets/energinet/prognosis/ENetNEA/wind_speed_10m.parquet"
-# ws100_path = "/datasets/energinet/prognosis/ENetNEA/wind_speed_100m.parquet"
+# windmill_same_cond_path = "./data/windmills_same_cond.csv"
+# settlement_12_path = "./data/ITU_DATA/settlement/201819_12_cleaned.parquet"
+
+# ws10_c_path = "./data/ITU_DATA/prognosis/ENetNEA/ws_10m_cmp.parquet"
+# ws100_c_path = "./data/ITU_DATA/prognosis/ENetNEA/ws_100m_cmp.parquet"
+# wu10_path = "./data/ITU_DATA/prognosis/ENetNEA/wu_10m.parquet"
+# wv10_path = "./data/ITU_DATA/prognosis/ENetNEA/wv_10m.parquet"
+# wu100_path = "./data/ITU_DATA/prognosis/ENetNEA/wu_100m.parquet"
+# wv100_path = "./data/ITU_DATA/prognosis/ENetNEA/wv_100m.parquet"
+# tmp2_c_path = "./data/ITU_DATA/prognosis/ENetNEA/temperatur_2m_compress.parquet" 
+# tmp100_c_path = "./data/ITU_DATA/prognosis/ENetNEA/temperatur_100m_compress.parquet" 
 # wd10_path = "/datasets/energinet/prognosis/ENetNEA/wind_direction_10m.parquet"
 # wd100_path = "/datasets/energinet/prognosis/ENetNEA/wind_direction_100m.parquet" 
 # model_path = "./model.parquet"
 
 windmill_path = "data/windmill_cleaned.csv"
+windmill_one_path = "data/windmill_570714700000004070.csv"
+windmill_LW_600_path = "data/windmill_LW_600.csv"
+windmill_geo_analysis_path = "data/windmill_for_geoanalysis.csv"
+windmill_same_cond_path = "data/windmills_same_cond.csv"
+
 settlement_path = "data/ITU_DATA/settlement/201{8,9}.parquet"
-ws10_path = "data/ITU_DATA/prognosis/ENetNEA/wind_speed_10m.parquet"
-ws100_path = "data/ITU_DATA/prognosis/ENetNEA/wind_speed_100m.parquet"
-wd10_path = "data/ITU_DATA/prognosis/ENetNEA/wind_direction_10m.parquet"
-wd100_path = "data/ITU_DATA/prognosis/ENetNEA/wind_direction_100m.parquet" 
-wsgust_path = "data/ITU_DATA/observations/observed_wind_speed_gust.parquet" 
+settlement_2019_one_path = "data/ITU_DATA/settlement/2019_570714700000004070.parquet"
+settlement_201819_one_path = "data/ITU_DATA/settlement/201819_570714700000004070.parquet"
+settlement_2019_path = "data/ITU_DATA/settlement/2019_cleaned.parquet"
+settlement_12_path = "data/ITU_DATA/settlement/201819_12_cleaned.parquet"
+settlement_201812_path = "data/ITU_DATA/settlement/2018_12.parquet"
+settlement_201912_path = "data/ITU_DATA/settlement/2019_12_cleaned.parquet"
+# settlement_570714700000000362_path = "data/ITU_DATA/settlement/2019_570714700000000362.parquet"
+
+wu10_path = "data/ITU_DATA/prognosis/ENetNEA/wu_10m.parquet"
+wv10_path = "data/ITU_DATA/prognosis/ENetNEA/wv_10m.parquet"
+wu100_path = "data/ITU_DATA/prognosis/ENetNEA/wu_100m.parquet"
+wv100_path = "data/ITU_DATA/prognosis/ENetNEA/wv_100m.parquet"
+
+# wui10_path = "data/ITU_DATA/prognosis/ENetNEA/ws_ui_10m.parquet"
+# wvi10_path = "data/ITU_DATA/prognosis/ENetNEA/ws_vi_10m.parquet"
+# wui100_path = "data/ITU_DATA/prognosis/ENetNEA/ws_ui_100m.parquet"
+# wvi100_path = "data/ITU_DATA/prognosis/ENetNEA/ws_vi_100m.parquet"
+
+# ws10_path = "data/ITU_DATA/prognosis/ENetNEA/wind_speed_10m.parquet"
+# ws100_path = "data/ITU_DATA/prognosis/ENetNEA/wind_speed_100m.parquet"
+# wd10_path = "data/ITU_DATA/prognosis/ENetNEA/wind_direction_10m.parquet"
+# wd100_path = "data/ITU_DATA/prognosis/ENetNEA/wind_direction_100m.parquet" 
+
 tmp2_path = "data/ITU_DATA/prognosis/ENetNEA/temperatur_2m.parquet" 
 tmp100_path = "data/ITU_DATA/prognosis/ENetNEA/temperatur_100m.parquet" 
-model_path = "model.parquet"
+
+ws10_path = "data/ITU_DATA/prognosis/ENetNEA/ws_10m_cmp.parquet"
+ws100_path = "data/ITU_DATA/prognosis/ENetNEA/ws_100m_cmp.parquet"
+wd10_path = "data/ITU_DATA/prognosis/ENetNEA/wind_direction_10m_compress.parquet"
+wd100_path = "data/ITU_DATA/prognosis/ENetNEA/wind_direction_100m_compress.parquet" 
+tmp2_path = "data/ITU_DATA/prognosis/ENetNEA/temperatur_2m_compress.parquet" 
+tmp100_path = "data/ITU_DATA/prognosis/ENetNEA/temperatur_100m_compress.parquet" 
+
+# model_path = "model.parquet"
 
 
 settlement_schema = StructType([
     StructField("GSRN", StringType(), False),
     StructField("VAERDI", StringType(), False),
+    StructField("TIME_CET", StringType(), False)
+])
+settlement_schema_2 = StructType([
+    StructField("GSRN", StringType(), False),
+    StructField("VAERDI", DoubleType(), False),
     StructField("TIME_CET", StringType(), False)
 ])
 
@@ -34,14 +77,40 @@ windmills_schema = StructType([
     StructField("Placement",StringType(),True),
     StructField("UTM_x",StringType(),True),
     StructField("UTM_y",StringType(),True),
-    StructField("Capacity_kw", DecimalType(10,3), True),
-    StructField("Rotor_diameter", DecimalType(10,3), True),
-    StructField("Navhub_height", DecimalType(3), True),
-    StructField("Out_service", StringType(), True),
+    StructField("Capacity_kw", DoubleType(), True),
+    StructField("Rotor_diameter", DoubleType(), True),
+    StructField("Navhub_height", DoubleType(), True),
     StructField("grid", StringType(), True),
     StructField("grid_in_range",StringType(),True),
-    StructField("Land_cover", DecimalType(10,3), True),
-    StructField("Slope", DecimalType(10,3), True),
-    StructField("Elevation", DecimalType(10,3), True),
-    StructField("Roughness", DecimalType(10,3), True)
-])						
+    StructField("Land_cover", DoubleType(), True),
+    StructField("Slope", DoubleType(), True),
+    StructField("Elevation", DoubleType(), True),
+    StructField("Roughness", DoubleType(), True)
+])
+
+
+windmills_schema_2 = StructType([
+    StructField("GSRN",StringType(),False),
+    StructField("Capacity_kw", DoubleType(), True),
+    StructField("Rotor_diameter", DoubleType(), True),
+    StructField("Navhub_height", DoubleType(), True),
+    StructField("Slope", DoubleType(), True),
+    StructField("Elevation", DoubleType(), True),
+    StructField("Roughness", DoubleType(), True),
+    StructField("grid", StringType(), True),
+])	
+
+windmills_schema_3 = StructType([
+    StructField("GSRN",StringType(),False),
+    StructField("Turbine_type",StringType(),True),
+    StructField("Placement",StringType(),True),
+    StructField("UTM_x",StringType(),True),
+    StructField("UTM_y",StringType(),True),
+    StructField("Capacity_kw", DoubleType(), True),
+    StructField("Rotor_diameter", DoubleType(), True),
+    StructField("Navhub_height", DoubleType(), True),
+    StructField("grid", StringType(), True),
+    StructField("Slope", DoubleType(), True),
+    StructField("Elevation", DoubleType(), True),
+    StructField("Roughness", DoubleType(), True)
+])
--- tools.py
+++ tools.py
@@ -4,85 +4,141 @@ from math import log,sqrt,atan2,pi,cos,sin
 from pyspark.sql.functions import udf, sum, max, avg, first
 from pyspark.sql.types import *
 
-pow_law = lambda v2, z2, z_hat, a : v2 * ( (z_hat/z2) ** a) + 1e-06
+pow_law = lambda v, z, z_hat, a : v * ( (z_hat/z) ** a) + 1e-06
+linear_law = lambda x1, x2, y1, y2, z: ((y2 - y1) / (x2 - x1)) * z
+udic = {}
 
 def wind_speed_to_vector(s, d):
     u = s * cos(pi/180 * (270-d))
     v = s * sin(pi/180 * (270-d))
     return u, v
 
-def _pow_interpolation(v1, v2, z1, z2, z_hat):
+def _pow_interp(v1, v2, z1, z2, z_hat):
     a1 = abs(v1)
     a2 = abs(v2)
     if v1 < 0 and v2 >= 0 or v1 >= 0 and v2 < 0:
         a2 = a1 + a2
-    # elif s1 >= 0 and s2 < 0:
-    #     a2 = a1 + a2
 
     a = log(a2/(a1+1e-06)+1e-06) / log(z2/z1)
-    
-    return round(pow_law(v2, z2, z_hat, a),3)
+    return round(pow_law(v1, z1, z_hat, a),3)
 
-def wind_interp(s1, d1, s2, d2, z_hat):
-    u1, v1 = wind_speed_to_vector(s1, d1)
-    u2, v2 = wind_speed_to_vector(s2, d2)
-    u_i = _pow_interpolation(u1, u2, 10, 100, z_hat)
-    v_i = _pow_interpolation(v1, v2, 10, 100, z_hat)
+
+def wind_interp(u1, v1, u2, v2, z_hat):
+    u_i = round(_pow_interp(u1, u2, 10, 100, z_hat),3)
+    v_i = round(_pow_interp(v1, v2, 10, 100, z_hat),3)
     return u_i, v_i
 
+def wind_interp_pow(u, v, z_hat, rl):
+    u_i = round(pow_law(u, 10, z_hat, rl),3)
+    v_i = round(pow_law(v, 10, z_hat, rl),3)
+    return u_i, v_i
 
-cols_basic = ["GSRN", "TIME_CET", "Turbine_type", "Placement", "Capacity_kw", "Rotor_diameter", "Navhub_height", "Slope", "roughness", "VAERDI", "grid"]
+# def wind_interp_linear(u1, v1, u2, v2, z_hat):
+#     u_i = round(linear_law(10, 100, u1, u2, z_hat),3)
+#     v_i = round(linear_law(10, 100, v1, v2, z_hat),3)
+#     return u_i, v_i
 
-def _udf_by_grid(df:pd.DataFrame, type = FloatType()):
-    return udf(lambda g, t: list(df[df['TIME'] == t[:14]+'00:00'][g])[0] if df['TIME'].astype(str).str.contains(t[:14]+'00:00').any() else 0, type)
 
-def _udf_by_ws():
-    schema = StructType([
-        StructField("u_interp", FloatType(), True),
-        StructField("v_interp", FloatType(), True)
+# cols_basic = ["GSRN", "TIME_CET", "Capacity_kw", "Rotor_diameter", "Navhub_height", "Slope", "Roughness", "Elevation", "VAERDI", "grid"]
+# cols_basic = ["GSRN","TIME_CET", "Navhub_height", "Roughness", "VAERDI", "grid"]
+cols_ext = ["hour", "wu10", "wv10", "tmp2", "tmp100"]
+
+def _udf_by_grid(df:pd.DataFrame, tp=DoubleType()):
+    return udf(lambda g, t: df[df['TIME'] == t[:14]+'00:00'][g].tolist()[0] if df['TIME'].astype(str).str.contains(t[:14]+'00:00').any() else 0, tp)
+
+
+uv_schema = StructType([
+        StructField("u_interp", DoubleType(), True),
+        StructField("v_interp", DoubleType(), True)
     ])
-    return udf(lambda s1, d1, s2, d2, z: wind_interp(s1, d1, s2, d2, z), schema)
 
-udic={}
-def get_udf(ws10_dic,ws100_dic,wd10_dic,wd100_dic,tmp2_dic,tmp100_dic):
-    udic["type"] = udf(lambda x: {"H": 1.0, "W": 2.0, "P": 3.0, "M": 4.0}.get(x, 0.0), FloatType())
-    udic["placement"] = udf(lambda x: {"LAND": 1.0, "HAV": 2.0}.get(x, 0.0), FloatType())
-    udic["month"] = udf(lambda x: int(x[5:7]), IntegerType())
+# def get_udf(ws10_dic, ws100_dic, wu10_dic,wv10_dic,wu100_dic,wv100_dic,tmp2_dic,tmp100_dic):
+#     # udic["type"] = udf(lambda x: {"H": 1, "W": 2, "P": 3, "M": 4}.get(x, 0), IntegerType())
+#     # udic["placement"] = udf(lambda x: {"LAND": 1, "HAV": 2}.get(x, 0), IntegerType())
+#     udic["month"] = udf(lambda x: int(x[5:7]), IntegerType())
+#     udic["hour"] = udf(lambda x: int(x[11:13]), IntegerType())
+#     udic["mins"] = udf(lambda x: int(x[14:16]), IntegerType())
+#     udic["ws10"]  = _udf_by_grid(ws10_dic)
+#     udic["ws100"]  = _udf_by_grid(ws100_dic)
+#     udic["wu10"]  = _udf_by_grid(wu10_dic)
+#     udic["wv10"]  = _udf_by_grid(wv10_dic)
+#     udic["wu100"]  = _udf_by_grid(wu100_dic)
+#     udic["wv100"]  = _udf_by_grid(wv100_dic)
+#     udic["tmp2"]  = _udf_by_grid(tmp2_dic)
+#     udic["tmp100"]  = _udf_by_grid(tmp100_dic)
+#     udic["ws_interp_pow"]  = udf(lambda v, z, rl: round(pow_law(v, 10, z, rl),3), DoubleType())
+#     udic["wuv_interp_pow"]  = udf(lambda u, v, z, rl: wind_interp_pow(u, v, z, rl), uv_schema)
+#     udic["ws_interp_linear"]  = udf(lambda u1, v1, u2, v2, z: wind_interp_linear(u1, v1, u2, v2, z), uv_schema)
+#     udic["wuv_interp"]  = udf(lambda u1, v1, u2, v2, z: wind_interp(u1, v1, u2, v2, z), uv_schema)
+#     # udic["tmp_interp"]  = udf(lambda v1, v2, z_hat: _pow_interp(v1, v2, 2, 100, z_hat), DoubleType())
+#     udic["ws_hub"] = udf(lambda u, v: round((u ** 2 + v ** 2) ** 0.5, 3), DoubleType())
+#     udic["ws_^2"] = udf(lambda x: round(x ** 2, 3), DoubleType())
+#     udic["ws_^3"] = udf(lambda x: round(x ** 3, 3), DoubleType())
+
+def get_udf(wu10_dic,wv10_dic,tmp2_dic,tmp100_dic):
     udic["hour"] = udf(lambda x: int(x[11:13]), IntegerType())
-    udic["mins"] = udf(lambda x: int(x[14:16]), IntegerType())
-    udic["ws10"]  = _udf_by_grid(ws10_dic, FloatType())
-    udic["ws100"]  = _udf_by_grid(ws100_dic, FloatType())
-    udic["wd10"]  = _udf_by_grid(wd10_dic, IntegerType())
-    udic["wd100"]  = _udf_by_grid(wd100_dic, IntegerType())
-    udic["tmp2"]  = _udf_by_grid(tmp2_dic, FloatType())
-    udic["tmp100"]  = _udf_by_grid(tmp100_dic, FloatType())
-    udic["ws_interp"]  = _udf_by_ws()
+    udic["wu10"]  = _udf_by_grid(wu10_dic)
+    udic["wv10"]  = _udf_by_grid(wv10_dic)
+    udic["tmp2"]  = _udf_by_grid(tmp2_dic)
+    udic["tmp100"]  = _udf_by_grid(tmp100_dic)
 
+# def _normal_f_extract(df):
+#     return df.withColumn("month", udic["month"](df.TIME_CET)) \
+#                 .withColumn("hour", udic["hour"](df.TIME_CET)) \
+#                 .withColumn("mins", udic["mins"](df.TIME_CET)) \
+#                 .withColumn("ws10", udic["ws10"](df.grid, df.TIME_CET)) \
+#                 .withColumn("ws100", udic["ws100"](df.grid, df.TIME_CET)) \
+#                 .withColumn("wu10", udic["wu10"](df.grid, df.TIME_CET)) \
+#                 .withColumn("wv10", udic["wv10"](df.grid, df.TIME_CET)) \
+#                 .withColumn("wu100", udic["wu100"](df.grid, df.TIME_CET)) \
+#                 .withColumn("wv100", udic["wv100"](df.grid, df.TIME_CET)) \
+#                 .withColumn("tmp2", udic["tmp2"](df.grid, df.TIME_CET)) \
+#                 .withColumn("tmp100", udic["tmp100"](df.grid, df.TIME_CET))
 
 def _normal_f_extract(df):
-    return df.withColumn("month", udic["month"](df.TIME_CET)) \
-                .withColumn("hour", udic["hour"](df.TIME_CET)) \
-                .withColumn("mins", udic["mins"](df.TIME_CET)) \
-                .withColumn("ws10", udic["ws10"](df.grid, df.TIME_CET)) \
-                .withColumn("ws100", udic["ws100"](df.grid, df.TIME_CET)) \
-                .withColumn("wd10", udic["wd10"](df.grid, df.TIME_CET)) \
-                .withColumn("wd100", udic["wd100"](df.grid, df.TIME_CET)) \
+    return df.withColumn("hour", udic["hour"](df.TIME_CET)) \
+                .withColumn("wu10", udic["wu10"](df.grid, df.TIME_CET)) \
+                .withColumn("wv10", udic["wv10"](df.grid, df.TIME_CET)) \
                 .withColumn("tmp2", udic["tmp2"](df.grid, df.TIME_CET)) \
                 .withColumn("tmp100", udic["tmp100"](df.grid, df.TIME_CET))
-                
+
+# def _second_f_extract(df):
+#     df = df.withColumn("ws_hub", udic["ws_hub"](df.u_interp, df.v_interp))
+#     df = df.withColumn("ws_^2", udic["ws_^2"](df.ws_hub))
+#     df = df.withColumn("ws_^3", udic["ws_^3"](df.ws_hub)) 
+#     return df
+
 def aggregate(df, join_df):
-    df = df.join(join_df, on="GSRN").select(cols_basic)
-    df = _normal_f_extract(df) \
-        .withColumn("Turbine_type", udic["type"](df.Turbine_type)) \
-        .withColumn("Placement", udic["placement"](df.Placement))
+    df = df.join(join_df, on="GSRN").select(join_df.columns + ["TIME_CET", "VAERDI"])
+    df = _normal_f_extract(df)
     return df
 
-def interp(df, join_df):
-    cols = cols_basic + ["month", "hour", "mins","wsCol.u_interp", "wsCol.v_interp"]
+
+def interp_pow(df, join_df):
+    # cols = cols_basic + cols_ext + ["wsCol.u_interp", "wsCol.v_interp", "tmp_interp"]
+    # cols = join_df.columns + cols_basic + cols_ext + ["wsCol.u_interp", "wsCol.v_interp", "tmp_interp"]
     df = aggregate(df, join_df)
-    df = df.withColumn("wsCol", \
-                udic["ws_interp"](df.ws10, df.wd10, df.ws100, df.wd100, df.Navhub_height)) \
+    df = df.withColumn("wsCol", udic["wuv_interp_pow"](df.wu10, df.wv10, df.Navhub_height, df.Roughness))
+    cols = df.columns
+    cols.remove("wsCol")
+    df = df.select(cols + ["wsCol.u_interp", "wsCol.v_interp"])
+    # .withColumn("tmp_interp", udic["tmp_interp"](df.tmp2, df.tmp100, df.Navhub_height))
+    return df
+
+# def interp_linear(df, join_df):
+#     cols = cols_basic + cols_ext + ["wsCol.u_interp", "wsCol.v_interp", "tmp_interp"]
+#     df = aggregate(df, join_df)
+#     df = df.withColumn("wsCol", udic["ws_interp_linear"](df.wu10, df.wv10, df.wu100, df.wv100, df.Navhub_height)) \
+#                 .select(cols)
+#                 # .withColumn("tmp_interp", udic["tmp_interp"](df.tmp2, df.tmp100, df.Navhub_height)) \
+#     return df
+
+def interp(df, join_df):
+    cols = cols_basic + cols_ext + ["wsCol.u_interp", "wsCol.v_interp", "tmp_interp"]
+    df = aggregate(df, join_df)
+    df = df.withColumn("wsCol", udic["wuv_interp"](df.ws10, df.wd10, df.ws100, df.wd100, df.Navhub_height)) \
                 .select(cols)
+                # .withColumn("tmp_interp", udic["tmp_interp"](df.tmp2, df.tmp100, 2, 100, df.Navhub_height)) \
     return df
 
 def upscaling(df, join_df):
@@ -90,7 +146,7 @@ def upscaling(df, join_df):
     # cols = cols_basic + ["month", "hour", "mins"]
     df = df.groupby("TIME_CET", "grid") \
             .agg(first("Slope").alias("Slope"), \
-            first("roughness").alias("roughness"), \
+            first("Roughness").alias("Roughness"), \
             sum("VAERDI").alias("VAERDI"))
             # .select(cols)
     df = _normal_f_extract(df)
